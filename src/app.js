e86032b2460a34f2ff20c91d50e69597e3f31540
Mohammad Aamir <aamireverlasting786>
2025-12-25 21:50:18 +0530

    Initial backend implementation for expense sharing

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import userRoutes from "./routes/user.routes.js";
+import groupRoutes from "./routes/group.routes.js";
+import expenseRoutes from "./routes/expense.routes.js";
+import balanceRoutes from "./routes/balance.routes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/groups", groupRoutes);
+app.use("/expenses", expenseRoutes);
+app.use("/balances", balanceRoutes);
+
+export default app;
